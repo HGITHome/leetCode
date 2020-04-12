9628c316e7fa3727bb4be7da031a34cdeaa13fe7
snail <hojuktong@outlook.com>
2020-04-12 16:01:46 +0800

    添加算法

---
+++ src/main/LeetCode16.java
@@ -0,0 +1,27 @@
+public class LeetCode16 {
+
+    public static void main(String[] args) {
+        //暴力破解法
+        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
+        int max = 0,l = 0 , r = height.length - 1;
+//        for (int i = 0 ; i < height.length ; i ++){
+//            for (int j = i +1 ; j < height.length ; j ++){
+//                int min = Math.min(height[j] , height[i]);
+//                int indexlen = j - i;
+//                int temp = min * indexlen;
+//                if(temp > max){
+//                    max = temp;
+//                }
+//            }
+//        }
+        //双指针法
+        while (l < r){
+            max = Math.max(max , Math.min(height[l],height[r]) * (r - l));
+            if(height[l] < height[r]){
+                l ++;
+            } else
+                r --;
+        }
+        System.out.println(max);
+    }
+}
